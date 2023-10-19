8be44ec7351fcd98f559527bfc83cff8f3482849
Mohanad Ahmed <mhndahmd031@gmail.com>
2023-10-19 16:44:40 +0300

    modified header comment

--- CS213-2023-20220012-20220034-20220348.cpp
+++ CS213-2023-20220012-20220034-20220348.cpp
@@ -1,20 +1,21 @@
 // FCAI – OOP Programming – 2023 - Assignment 1
 // Program Name: CS213-2023-20220012-20220034-20220348.cpp
-// Last Modification Date:	13/9/2023
+// Last Modification Date:	19/10/2023
 
 // Author1 : Ahmed Ehab Shehata Ali
-// Author1's ID : 20220012
-// Author1's email : am8122004@gmail.com
+// ID : 20220012
+// email : am8122004@gmail.com
 
 // Author2 : Ahmed Mohamed Amer Ahmed
-// Author1's ID : 20220034
-// Author's email : ahmedamer42214953@gmail.com
+// ID : 20220034
+// email : ahmedamer42214953@gmail.com
 
 // Author3 : Mohanad Abdullrahem Abdullrahman
-// Author1's ID : 20220348
-// Author3's email : mhndahmd031@gmail.com
+// ID : 20220348
+// email : mhndahmd031@gmail.com
 
-// Purpose:..........
+// Purpose: Program that loads a gray scale image and apply 15
+//          filters to it and store the result in another file.
 
 #include <iostream>
 #include <fstream>
@@ -345,12 +346,12 @@ void darkenAndLightenImage()
 void detectImageEdges()
 {
   int i, j;
-  //make the filteredimage black after each filter
-  for(int i=0 ; i<SIZE ; i++)
+  // make the filteredimage black after each filter
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < SIZE; j++)
     {
-    for(int j=0 ; j<SIZE ; j++)
-    {
-        filteredImage[i][j]=0;
+      filteredImage[i][j] = 0;
     }
   }
 
@@ -367,22 +368,22 @@ void detectImageEdges()
         }
         else if (image[i][j - 1] < 127 && image[i][j] >= 127)
         {
-          filteredImage[i][j - 1]  = 0;
+          filteredImage[i][j - 1] = 0;
           filteredImage[i][j] = 255;
         }
       }
       if (i > 0)
       {
-          if (image[i - 1][j] >= 127 && image[i][j] < 127)
-          {
-            filteredImage[i - 1][j] = 255;
-            filteredImage[i][j] = 0;
-          }
-          else if (image[i - 1][j] < 127 && image[i][j] >= 127)
-          {
-            filteredImage[i - 1][j] = 0;
-            filteredImage[i][j] = 255;
-          }
+        if (image[i - 1][j] >= 127 && image[i][j] < 127)
+        {
+          filteredImage[i - 1][j] = 255;
+          filteredImage[i][j] = 0;
+        }
+        else if (image[i - 1][j] < 127 && image[i][j] >= 127)
+        {
+          filteredImage[i - 1][j] = 0;
+          filteredImage[i][j] = 255;
+        }
       }
       else
         filteredImage[i][j] = 255;
@@ -791,37 +792,44 @@ void cropImage()
 }
 
 // Filter e
-void skewImageRight() {
-    cout<<"Please enter degree to skew right : ";
-    int degree ;
-    cin>>degree;
-    // convert from degree to radian
-    double radian = (degree/180.0)*M_PI;
-    // the space that the image will skew
-    double mov = SIZE * tan(radian);
-    double step = mov/SIZE ;
-    // the new width of the image
-    int width = int(SIZE - mov);
-    // make the background white
-     for(int i=0 ; i<SIZE ; i++){
-        for(int j=0 ; j<SIZE ; j++){
-            filteredImage[i][j]=255;
-        }
+void skewImageRight()
+{
+  cout << "Please enter degree to skew right : ";
+  int degree;
+  cin >> degree;
+  // convert from degree to radian
+  double radian = (degree / 180.0) * M_PI;
+  // the space that the image will skew
+  double mov = SIZE * tan(radian);
+  double step = mov / SIZE;
+  // the new width of the image
+  int width = int(SIZE - mov);
+  // make the background white
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < SIZE; j++)
+    {
+      filteredImage[i][j] = 255;
     }
-    // shrinking the image;
-    unsigned char cpyimage[SIZE][SIZE];
-    for(int i=0 ; i<SIZE ; i++){
-        for(int j=0 ; j<width ; j++){
-            cpyimage[i][j]=image[i][int(j/(1-tan(radian)))];
-        }
+  }
+  // shrinking the image;
+  unsigned char cpyimage[SIZE][SIZE];
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      cpyimage[i][j] = image[i][int(j / (1 - tan(radian)))];
     }
-    // the image with skew
-    for(int i=0 ; i<SIZE ; i++){
-        for(int j=0 ; j<width ; j++){
-            filteredImage[i][j+int(mov)] = cpyimage[i][j];
-        }
-        mov -=step;
+  }
+  // the image with skew
+  for (int i = 0; i < SIZE; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      filteredImage[i][j + int(mov)] = cpyimage[i][j];
     }
+    mov -= step;
+  }
 }
 
 // Filter f
